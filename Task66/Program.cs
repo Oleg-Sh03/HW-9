16df5daeb6d03cdd49acde40ffd74c9493d30a36
Oleg-Sh03 <120747149+Oleg-Sh03@users.noreply.github.com>
2023-02-15 20:20:16 +0300

    Добавлены решения задач

---
+++ Task66/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+// в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+void IncorrectValue()
+{
+Console.WriteLine("Введено некорректное значение");
+Environment.Exit(0);
+}
+
+int UserInput()
+{
+if (!int.TryParse(Console.ReadLine(), out int temp)) IncorrectValue();
+return temp;
+}
+
+int sumM = 0;
+int sumN = 0;
+int sumE = 0;
+void IntegerSum(int numM, int numN)
+{
+if (numM < numN)
+{
+sumM = sumM + numM;
+IntegerSum(numM + 1, numN);
+}
+else if (numM > numN)
+{
+sumN = sumM + numM;
+IntegerSum(numM - 1, numN);
+}
+else if (numM == numN)
+{
+    sumE = numM;
+}
+
+}
+
+Console.Write("Введите целое число M: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+int number1 = UserInput();
+Console.Write("Введите целое число N: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+int number2 = UserInput();
+IntegerSum(number1, number2);
+Console.WriteLine($"M = {number1}; N = {number2} -> {sumM + sumN + sumE}");
+
---
+++ Task68/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Введите число m: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int A(int m, int n)
+{
+  if (m == 0)
+    return n + 1;
+  else
+    if ((m != 0) && (n == 0))
+      return A(m - 1, 1);
+    else
+      return A(m - 1, A(m, n - 1));
+} 
+int result = A(number1, number2);
+Console.WriteLine($"m = {number1}, n = {number2} -> A(m,n) = {result}");
